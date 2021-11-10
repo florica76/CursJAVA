f227d8130f22f33e177dd3f360bdb15adad2d205
Florin Ivan <florica76@gmail.com>
2021-11-10 22:07:44 +0200

    .

--- src/curs1/Tester.java
+++ src/curs1/Tester.java
@@ -5,20 +5,21 @@ public class Tester {
 	
 	String nume;
 	int ratePerHour;
-	
-	
 
-	
 	public String getNume() {
+
 		return nume;
 	}
 	public void setNume(String nume) {
+
 		this.nume = nume;
 	}
 	public int getRatePerHour() {
+
 		return ratePerHour;
 	}
 	public void setRatePerHour(int ratePerHour) {
+
 		this.ratePerHour = ratePerHour;
 	}
 	
