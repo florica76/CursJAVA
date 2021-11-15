0b9eef659396b86554591b02570e77d95231266f
Florin Ivan <florica76@gmail.com>
2021-11-15 19:20:10 +0200

    Tema curs 1.

--- src/curs1/WageCalculator.java
+++ src/curs1/WageCalculator.java
@@ -9,12 +9,15 @@ public class WageCalculator {
 		tester1.setNume("Oana");
 		tester1.setRatePerHour(40);
 		tester1.setAge(25);
+
 		System.out.println(tester1.getNume());
+
 
 		Tester tester2 = new Tester();
 		tester2.setNume("Maria");
 		tester2.setRatePerHour(25);
 		tester2.setAge(40);
+
 		System.out.println(tester2.getNume());
 		
 		WageCalculator calc = new WageCalculator();
