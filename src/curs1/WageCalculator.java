70ef920285247493677ccb27be0714b800d8210e
Florin Ivan <florica76@gmail.com>
2021-11-15 19:18:31 +0200

    Tema, cust 1

--- src/curs1/WageCalculator.java
+++ src/curs1/WageCalculator.java
@@ -20,9 +20,9 @@ public class WageCalculator {
 		WageCalculator calc = new WageCalculator();
 		
 		System.out.println("Salariul lui " + tester1.getNume() + " este: " + calc.calculateSalary(tester1.getRatePerHour()));
-		System.out.println(tester1.getNume() + " are " + tester1.age + " ani.");
+		System.out.println(tester1.getNume() + " are " + tester1.getAge() + " ani.");
 		System.out.println("Salariul lui " + tester2.getNume() + " este: " + calc.calculateSalary(tester2.getRatePerHour()));
-		System.out.println(tester2.getNume() + " are " + tester2.age + " ani.");
+		System.out.println(tester2.getNume() + " are " + tester2.getAge() + " ani.");
 	}
 	
 	public int calculateSalary(int rateHurly) {
