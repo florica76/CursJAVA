fbc52c93b7e20a3073c02a4eb181e4134a9533a8
Florin Ivan <florica76@gmail.com>
2021-11-23 09:28:44 +0200

    Tema curs 2 correct typo

--- src/Curs2/Shape.java
+++ src/Curs2/Shape.java
@@ -2,19 +2,19 @@ package Curs2;
 
 public class Shape {
 
-    public Shape(int lenght) {
-        System.out.println("The area of a square is : " + calculateRectangleArea(lenght,lenght));
+    public Shape(int length) {
+        System.out.println("The area of a square is : " + calculateRectangleArea(length,length));
     }
 
-    public Shape(int lenght,int width){
-        System.out.println("The area of a rectangle is : " + calculateRectangleArea(lenght,width));
+    public Shape(int length,int width){
+        System.out.println("The area of a rectangle is : " + calculateRectangleArea(length,width));
     }
 
     public Shape(double radius){
         System.out.println("The area of a circle is : " + calculateCircleArea(radius));
     }
-    int calculateRectangleArea(int lenght, int width) {
-        return lenght * width;
+    int calculateRectangleArea(int length, int width) {
+        return length * width;
     }
 
     double calculateCircleArea(double radius) {
