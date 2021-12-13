d4810bb2e9aa3408d96cdbdd29d0c816ed505486
Florin Ivan <florica76@gmail.com>
2021-12-13 18:47:07 +0200

    .

--- src/Curs3/StaticVsNonStatic.java
+++ src/Curs3/StaticVsNonStatic.java
@@ -1,43 +1,24 @@
 package Curs3;
 
 public class StaticVsNonStatic {
-
     //Instance area
     public String nume = "Jhonny ";
-
     public String printeazaMesaj() {
         return "Salut " + prenume;
     }
-
-
     //end of instance area
 
     //static area
     public static String prenume = "Bravo";
-
-
     public static String printeazaMesajStatic() {
-
         return "Hello static " + prenume;
     }
-
-
     //end of static area
-
-
-
     public static void main(String[] args) {
-
-
         StaticVsNonStatic obj =  new StaticVsNonStatic();
-
         System.out.println("Numele meu este " + obj.nume + prenume);
-
         System.out.println(printeazaMesajStatic());
-
         System.out.println(obj.printeazaMesaj());
-
-
     }
 
 }
--- src/Curs3/StaticVsNonStatic2.java
+++ src/Curs3/StaticVsNonStatic2.java
@@ -1,37 +1,25 @@
 package Curs3;
 public class StaticVsNonStatic2 {
-
     public static String nume;
-
     public static String departament = "QA";
-
-
-
-
     public static void main(String[] args) {
-
         //StaticVsNonStatic obj =  new StaticVsNonStatic();
         //System.out.println(obj.printeazaMesaj());
-
         //System.out.println(StaticVsNonStatic.printeazaMesajStatic());
-
         //obiect 1
         StaticVsNonStatic2  person =  new StaticVsNonStatic2();
         person.nume = "Jhonny ";
         System.out.println(person.nume + departament);
-
         //obiect 2
         StaticVsNonStatic2 person2 = new StaticVsNonStatic2();
         person2.nume = "Bravo ";
         System.out.println(person2.nume + departament);
         System.out.println("++" +person.nume + departament);
-
         departament = "DEV";
         //obiect 3
         StaticVsNonStatic2 person3 = new StaticVsNonStatic2();
         person3.nume = "Trei ";
         System.out.println(person3.nume + departament);
-
         ///
         System.out.println("-------------------------------");
         System.out.println(person.nume + departament);
