7714b25b7baf572f07021a1e3671d5ead59504ce
Florin Ivan <florica76@gmail.com>
2021-12-14 09:16:36 +0200

    Curs4

--- src/Curs3/Car.java
+++ src/Curs3/Car.java
@@ -1,5 +1,19 @@
 package Curs3;
 
-    public class Car {
+public class Car {
+        static String brand;
+        static String type;
+        static String engine;
+
+    public Car (String brand, String engine){
+        this.brand = brand;
+        this.engine= engine;
+        type = "Suv";
+    }
+        static String carDetails(){
+
+        return "All " + brand + " cars are " + engine + " and are of type " + type;
+
+    }
         }
 
--- src/Curs3/TestCar.java
+++ src/Curs3/TestCar.java
@@ -1,4 +1,7 @@
 package Curs3;
 
 public class TestCar {
+    public static void main(String[] args) {
+
+    }
 }
